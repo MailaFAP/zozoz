f37798421d52b669a304c9f539a4db0675976245
unknown <mirandamartins05@gmail.com>
2024-02-05 10:54:20 -0300

    adicionado footer

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -1,18 +1,35 @@
 import React from "react";
-import ContatoFoooter from "../ContatoFooter";
-import Localizacao from "../Localizacao";
-import Social from "../Social";
+import { PhoneIcon } from "@chakra-ui/icons";
+import Instagram from "../../imagens/instagram.png"
 
 function Footer(){
     return (
-        <>
-        <ContatoFoooter />
-        <Localizacao />
-        <Social />
-        <hr/>
+        <div className="footer">
+            <div className="contato-footer">
+                <h1>Contato</h1>
+                <PhoneIcon />
+                <h4>(11)97421-0911</h4>
+            </div>
 
-        <p>Desenvolvido por : Amanda Araújo | Maila Ferreira | Myllena Miranda Martins</p>
-        </>
+            <div className="localização-footer">
+                <h1>Localização</h1>
+                <h4>Rua: Bernardino de Sena , 107
+                Casa Verde
+                São Paulo</h4>
+            </div>
+
+            <div className="social-footer">
+                <h1>Social</h1>
+                <h4>
+                    Siga nosso perfil nas redes sociais
+                </h4>
+                <img src={Instagram} width="20px" />
+            </div>
+
+            <hr/>
+
+            <p>Desenvolvido por : Amanda Araújo | Maila Ferreira | Myllena Miranda Martins</p>
+        </div>
     )
 }
 
