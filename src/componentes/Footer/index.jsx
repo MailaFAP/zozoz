0b08f2f43b885ebda229c22b9a3d2f56c36613b6
amandascam03 <amandascam03@gmail.com>
2024-02-07 15:38:30 -0300

    ajustes nos erros de espaçamento, imagem e ícone na página contato

--- src/componentes/Footer/index.jsx
+++ src/componentes/Footer/index.jsx
@@ -1,51 +1,67 @@
-import React from "react";
 import { PhoneIcon } from "@chakra-ui/icons";
-import Instagram from "../../imagens/insta.png"
-import "./Footer.css"
+import Instagram from "../../imagens/insta.png";
+import "./Footer.css";
 import Logo from "../Logo";
-import LogoFooter from "../../imagens/logofooter.jpg"
+import LogoFooter from "../../imagens/logofooter.jpg";
 import { Link } from "react-router-dom";
-import { Box } from "@chakra-ui/layout";
+import { Circle, HStack, Image } from "@chakra-ui/react";
 
 function Footer() {
-    return (
-        <div className="footer">
-            <footer>
-                <div className="logoFooter">
-                    <Logo imagem={LogoFooter} size="8rem" />
-                </div>
-                <div className="contato-footer">
-                    <h2>Contato</h2>
-                    <PhoneIcon />
-                    <h4>(11)97421-0911</h4>
-                </div>
-
-                <div className="localização-footer">
-                    <h2>Localização</h2>
-                    <h4>Rua: Bernardino de Sena , 107<br />
-                        Casa Verde<br />
-                        São Paulo
-                    </h4>
-                </div>
-
-                <div className="social-footer">
-                    <h2>Social</h2>
-                    <div className="circulo">
-                        <Link to="https://www.instagram.com/estudio_zozoz?igsh=ZjRiYnZrdDB3YjBn" isExternal>
-                            <Box w="100px" h="100px">
-                                <img src={Instagram} width="15px" />
-                            </Box>
-                        </Link>
-                    </div>
-                </div>
-                <div className="devs">
-                    <hr />
-                    <p>Desenvolvido por : Amanda Araújo | Maila Ferreira | Myllena Miranda Martins</p>
-                </div>
-
-            </footer>
+  return (
+    <div className="footer">
+      <footer>
+        <div className="logoFooter">
+          <Logo imagem={LogoFooter} size="8rem" />
         </div>
-    )
+        <div className="contato-footer">
+          <h2>Contato</h2>
+          <HStack>
+              <Circle size="30px" bg="white" color="#92d8b6">
+                <PhoneIcon />
+              </Circle>
+          </HStack>
+          <h4>(11)97421-0911</h4>
+        </div>
+
+        <div className="localização-footer">
+          <h2>Localização</h2>
+          <h4>
+            Rua: Bernardino de Sena, 107
+            <br />
+            Casa Verde
+            <br />
+            São Paulo
+          </h4>
+        </div>
+
+        <div className="social-footer">
+          <h2>Social</h2>
+          {/* <div className="circulo">
+                    <Link to="https://www.instagram.com/estudio_zozoz?igsh=ZjRiYnZrdDB3YjBn" isExternal>
+                        <Image src={Instagram} />
+                    </Link>
+                    </div> */}
+          <HStack>
+            <Link
+              to="https://www.instagram.com/estudio_zozoz?igsh=ZjRiYnZrdDB3YjBn"
+              isExternal
+            >
+              <Circle size="30px" bg="white">
+                <Image src={Instagram} />
+              </Circle>
+            </Link>
+          </HStack>
+        </div>
+        <div className="devs">
+          <hr />
+          <p>
+            Desenvolvido por : Amanda Araújo | Maila Ferreira | Myllena Miranda
+            Martins
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
