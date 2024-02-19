812098b77d4f5be3fd1d0c0a111f5aace5ca76b7
amandascam03 <amandascam03@gmail.com>
2024-02-19 14:56:13 -0300

    iniciando pagina de servicos com tabela de servicos e precos, imagem e botao agendar

--- src/pages/Servicos/index.jsx
+++ src/pages/Servicos/index.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import Header from "../../componentes/Header";
+import { Box, Button, Flex, Image } from "@chakra-ui/react";
+import "./Servicos.css";
+import Tabela from "../../componentes/Tabela";
+import ImagemPrecos from "../../imagens/img-precos.png";
 
-function Servicos (){
-    return(
-        <div>
-            <Header />
-            Serviços
-        </div>
-    )
+function Servicos() {
+  return (
+    <div>
+      <Header />
+      <Flex p="112px 0" flexDir={"column"} gap={"50px"} alignItems={"center"}>
+        <Box>
+          <Tabela />
+        </Box>
+        <Box display={"flex"} justifyContent={"center"}>
+          <Image src={ImagemPrecos} h="270px" />
+        </Box>
+        <Button color={"white"} bgColor={"#d8b192"} width={"180px"} height={"50px"} borderRadius={"50px"}>Agendar</Button>
+      </Flex>
+    </div>
+  );
 }
 export default Servicos;
