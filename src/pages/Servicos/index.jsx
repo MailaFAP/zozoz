7afe5d3c33bf2782a348b83ee56c8fb79ae1fe1a
amandascam03 <amandascam03@gmail.com>
2024-02-21 17:25:23 -0300

    adição de imagens da parte de produtos na página servicos com media query

--- src/pages/Servicos/index.jsx
+++ src/pages/Servicos/index.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 import Header from "../../componentes/Header";
-import { Box, Button, Flex, Image } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Image,
+  Text,
+  useMediaQuery,
+} from "@chakra-ui/react";
 import "./Servicos.css";
 import Tabela from "../../componentes/Tabela";
 import ImagemPrecos from "../../imagens/img-precos.png";
@@ -8,12 +15,20 @@ import Cutilagem from "../../imagens/cutilagem.png";
 import Esmaltacao from "../../imagens/esmaltacao.png";
 import Esmaltes from "../../imagens/esmaltes.png";
 import BoxServicos from "../../componentes/BoxServicos";
+import UnhasAmarelo from "../../imagens/unhas-amarelo.png";
 
 function Servicos() {
+  const [isLargerThanMD] = useMediaQuery("(min-width: 768px)");
+
   return (
     <div>
       <Header />
-      <Flex p="112px 0 0 0" flexDir={"column"} gap={"50px"} alignItems={"center"}>
+      <Flex
+        p="112px 0 0 0"
+        flexDir={"column"}
+        gap={"50px"}
+        alignItems={"center"}
+      >
         <Box>
           <Tabela />
         </Box>
@@ -31,12 +46,78 @@ function Servicos() {
         </Button>
       </Flex>
       <Flex justifyContent={"center"} gap={"18px"} m={"100px 0"}>
-        <BoxServicos imagem={Cutilagem} titulo="Cutilagem" descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit." />
-
-        <BoxServicos imagem={Esmaltacao} titulo="Esmaltação" descricao="Esmaltação com foco em unhas naturais" />
-
-        <BoxServicos imagem={Esmaltes} titulo="Esmaltes" descricao="Utilizamos esmaltes diferenciados de ótima qualidade para um melhor resultado" />
+        <BoxServicos
+          imagem={Cutilagem}
+          titulo="Cutilagem"
+          descricao="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        />
+        <BoxServicos
+          imagem={Esmaltacao}
+          titulo="Esmaltação"
+          descricao="Esmaltação com foco em unhas naturais"
+        />
+        <BoxServicos
+          imagem={Esmaltes}
+          titulo="Esmaltes"
+          descricao="Utilizamos esmaltes diferenciados de ótima qualidade para um melhor resultado"
+        />
       </Flex>
+      {isLargerThanMD ? (
+        <Flex flexDir={"column"}>
+          <Text
+            fontSize={{ base: "24px", md: "40px" }}
+            color={"#d8b192"}
+            fontWeight={"600"}
+            textAlign={"center"}
+          >
+            PRODUTOS
+          </Text>
+          <Box display={"flex"} m={"50px 0"} gap={"20px"}>
+            <Box w={"50vw"} display={"flex"} justifyContent={"end"}>
+              <Image src={UnhasAmarelo} height={"400px"} />
+            </Box>
+
+            <Box display={"flex"} flexDir={"column"} gap={"20px"}>
+              <Box
+                w={"50vw"}
+                display={"flex"}
+                gap={"20px"}
+                justifyContent={"start"}
+                alignItems={"center"}
+              >
+                <Image src={UnhasAmarelo} height={"190px"} />
+                <Image src={UnhasAmarelo} height={"190px"} />
+              </Box>
+              <Box
+                w={"50vw"}
+                display={"flex"}
+                gap={"20px"}
+                justifyContent={"start"}
+                alignItems={"center"}
+              >
+                <Image src={UnhasAmarelo} height={"190px"} />
+                <Image src={UnhasAmarelo} height={"190px"} />
+              </Box>
+            </Box>
+          </Box>
+        </Flex>
+      ) : (
+        <Flex>
+          <Box
+            justifyContent={"center"}
+            display={"flex"}
+            flexDir={"row"}
+            flexWrap={"wrap"}
+            gap={"20px"}
+          >
+            <Image src={UnhasAmarelo} height={"200px"} />
+            <Image src={UnhasAmarelo} height={"200px"} />
+            <Image src={UnhasAmarelo} height={"200px"} />
+            <Image src={UnhasAmarelo} height={"200px"} />
+            <Image src={UnhasAmarelo} height={"200px"} />
+          </Box>
+        </Flex>
+      )}
     </div>
   );
 }
