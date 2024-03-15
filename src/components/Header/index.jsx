872c0a4a145b92519cef1c0b085fa84029bf71fe
MailaFAP <maila_3@hotmail.com>
2024-03-15 12:26:43 -0300

    retirei as funções da index.header que não estavam sendo utilizadas, e refatorei o código da Home para não repetir código nas imagens, criando uma props pra cada tipo de imagem

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,31 +6,23 @@ import MenuMobile from "../MenuMobile";
 import LogoMenu from "../../assets/logo.jpeg";
 
 function Header() {
-  const [isLargerThanMD] = useMediaQuery("(min-width: 768px)");
+	const [isLargerThanMD] = useMediaQuery("(min-width: 768px)");
 
-  return (
-    <Box position={"relative"}>
-      <HStack
-        bgColor="rgba(0, 0, 0, 0.3)"
-        w="full"
-        p={{ base: "1rem 3rem", md: "1rem 8rem" }}
-        justifyContent={"space-between"}
-        position={"fixed"}
-        zIndex={1}
-      >
-        <Logo imagem={LogoMenu} size="5rem" />
-        {isLargerThanMD ? <DesktopNavbar /> : <MobileNavbar />}
-      </HStack>
-    </Box>
-  );
-}
-
-function DesktopNavbar() {
-  return <Menu />;
-}
-
-function MobileNavbar() {
-  return <MenuMobile />;
+	return (
+		<Box position={"relative"}>
+			<HStack
+				bgColor="rgba(0, 0, 0, 0.3)"
+				w="full"
+				p={{ base: "1rem 3rem", md: "1rem 8rem" }}
+				justifyContent={"space-between"}
+				position={"fixed"}
+				zIndex={1}
+			>
+				<Logo imagem={LogoMenu} size="5rem" />
+				{isLargerThanMD ? <Menu /> : <MenuMobile />}
+			</HStack>
+		</Box>
+	);
 }
 
 export default Header;
